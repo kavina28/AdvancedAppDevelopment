c579d8a39cb860cfcafe3085b6a9de588d11997f
Kavinashri V <119046521+kavina28@users.noreply.github.com>
2024-02-15 10:25:33 +0530

    Add files via upload

---
+++ day14/src/main/java/com/bec/becenquiry/BecenquiryApplication.java
@@ -0,0 +1,13 @@
+package com.bec.becenquiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecenquiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BecenquiryApplication.class, args);
+	}
+
+}
---
+++ day14/src/test/java/com/bec/becenquiry/BecenquiryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bec.becenquiry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecenquiryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
